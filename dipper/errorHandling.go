80ecadeab9c10287c456a046abcc86ae3ac163d0
Chun Huang <Charles546@users.noreply.github.com>
2019-01-13 18:54:27 -0800

    adding stack trace to error handling

--- dipper/errorHandling.go
+++ dipper/errorHandling.go
@@ -1,9 +1,12 @@
 package dipper
 
+import "github.com/go-errors/errors"
+
 // SafeExitOnError : use this function in defer statement to ignore errors
 func SafeExitOnError(args ...interface{}) {
 	if r := recover(); r != nil {
 		log.Warningf("Resuming after error: %v", r)
+		log.Warning(errors.Wrap(r, 1).ErrorStack())
 		log.Warningf(args[0].(string), args[1:]...)
 	}
 }
