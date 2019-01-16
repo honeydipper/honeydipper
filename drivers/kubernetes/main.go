3bc76cbdbea7e8b160521f20a1db81050808cad7
Chun Huang <Charles546@users.noreply.github.com>
2019-01-16 14:59:05 -0800

    Feature: kubernetes to create job, wait for job, get job log Feature: allow long running command to override timeout control

--- drivers/kubernetes/main.go
+++ drivers/kubernetes/main.go
@@ -4,15 +4,22 @@ import (
 	"encoding/base64"
 	"flag"
 	"fmt"
+	"github.com/ghodss/yaml"
 	"github.com/honeyscience/honeydipper/dipper"
 	"github.com/op/go-logging"
+	"io/ioutil"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"os"
+	"strconv"
+	"time"
 )
 
 var log *logging.Logger
+var err error
 
 func init() {
 	flag.Usage = func() {
@@ -30,15 +37,145 @@ func main() {
 	driver = dipper.NewDriver(os.Args[1], "kubernetes")
 	log = driver.GetLogger()
 	driver.CommandProvider.Commands["recycleDeployment"] = recycleDeployment
+	driver.CommandProvider.Commands["createJob"] = createJob
+	driver.CommandProvider.Commands["waitForJob"] = waitForJob
+	driver.CommandProvider.Commands["getJobLog"] = getJobLog
 	driver.Reload = func(*dipper.Message) {}
 	driver.Run()
 }
 
-func recycleDeployment(m *dipper.Message) {
-	if log == nil {
-		log = driver.GetLogger()
+func getJobLog(m *dipper.Message) {
+	k8client := prepareKubeConfig(m)
+
+	nameSpace, ok := dipper.GetMapDataStr(m.Payload, "namespace")
+	if !ok {
+		nameSpace = "default"
 	}
-	m = dipper.DeserializePayload(m)
+	jobName := dipper.MustGetMapDataStr(m.Payload, "job")
+	search := metav1.ListOptions{
+		LabelSelector: "job-name==" + jobName,
+	}
+	search.Kind = "Pod"
+
+	client := k8client.CoreV1().Pods(nameSpace)
+	pods, err := client.List(search)
+	if err != nil || len(pods.Items) < 1 {
+		log.Panicf("[%s] unable to find the pod for the job %+v", driver.Service, err)
+	}
+
+	stream, err := client.GetLogs(pods.Items[0].Name, &corev1.PodLogOptions{}).Stream()
+	if err != nil {
+		log.Panicf("[%s] unable to fetch the logs for the pod %+v", driver.Service, err)
+	}
+
+	defer stream.Close()
+
+	podlog, err := ioutil.ReadAll(stream)
+	if err != nil {
+		log.Panicf("[%s] unable to fetch the logs from reader %+v", driver.Service, err)
+	}
+	m.Reply <- dipper.Message{
+		Payload: map[string]interface{}{
+			"log": string(podlog),
+		},
+	}
+
+}
+
+func waitForJob(m *dipper.Message) {
+	k8client := prepareKubeConfig(m)
+
+	nameSpace, ok := dipper.GetMapDataStr(m.Payload, "namespace")
+	if !ok {
+		nameSpace = "default"
+	}
+
+	jobName := dipper.MustGetMapDataStr(m.Payload, "job")
+	timeout := 10
+	timeoutStr, ok := dipper.GetMapDataStr(m.Payload, "timeout")
+	if ok {
+		timeout, _ = strconv.Atoi(timeoutStr)
+	}
+
+	jobclient := k8client.BatchV1().Jobs(nameSpace)
+	watchOption := metav1.ListOptions{
+		FieldSelector: "metadata.name==" + jobName,
+	}
+	watchOption.Kind = "Job"
+	jobstatus, err := jobclient.Watch(watchOption)
+	if err != nil {
+		log.Panicf("[%s] unable to watch the job %+v", driver.Service, err)
+	}
+
+	finalStatus := make(chan dipper.Message, 1)
+	go func() {
+		for evt := range jobstatus.ResultChan() {
+			if evt.Type == "ADDED" || evt.Type == "MODIFIED" {
+				job := evt.Object.(*batchv1.Job)
+				if len(job.Status.Conditions) > 0 && job.Status.Active == 0 {
+					finalStatus <- dipper.Message{
+						Payload: map[string]interface{}{
+							"status": job.Status,
+						},
+					}
+				}
+			}
+		}
+	}()
+
+	m.Reply <- dipper.Message{
+		Labels: map[string]string{
+			"no-timeout": "yes",
+		},
+	} // suppress timeout control
+
+	select {
+	case msg := <-finalStatus:
+		m.Reply <- msg
+		jobstatus.Stop()
+	case <-time.After(time.Duration(timeout) * time.Second):
+		jobstatus.Stop()
+		log.Panic("Time out")
+	}
+}
+
+func createJob(m *dipper.Message) {
+	k8client := prepareKubeConfig(m)
+
+	nameSpace, ok := dipper.GetMapDataStr(m.Payload, "namespace")
+	if !ok {
+		nameSpace = "default"
+	}
+
+	buf, err := yaml.Marshal(dipper.MustGetMapData(m.Payload, "job"))
+	if err != nil {
+		log.Panicf("[%s] unable to marshal job manifest %+v", driver.Service, err)
+	}
+
+	jobSpec := batchv1.Job{}
+	err = yaml.Unmarshal(buf, &jobSpec)
+	if err != nil {
+		log.Panicf("[%s] invalid job manifest %+v", driver.Service, err)
+	}
+	log.Debugf("[%s] source %+v job spec %+v", driver.Service, dipper.MustGetMapData(m.Payload, "job"), jobSpec)
+
+	jobclient := k8client.BatchV1().Jobs(nameSpace)
+	jobResult, err := jobclient.Create(&jobSpec)
+	if err != nil {
+		log.Panicf("[%s] failed to create job %+v", driver.Service, err)
+	}
+
+	m.Reply <- dipper.Message{
+		Payload: map[string]interface{}{
+			"metadata": jobResult.ObjectMeta,
+			"status":   jobResult.Status,
+		},
+	}
+}
+
+func recycleDeployment(m *dipper.Message) {
+	k8client := prepareKubeConfig(m)
+
 	deploymentName, ok := dipper.GetMapDataStr(m.Payload, "deployment")
 	log.Infof("[%s] got deploymentName %s", driver.Service, deploymentName)
 	if !ok {
@@ -47,29 +184,6 @@ func recycleDeployment(m *dipper.Message) {
 	nameSpace, ok := dipper.GetMapDataStr(m.Payload, "namespace")
 	if !ok {
 		nameSpace = "default"
-	}
-	source, ok := dipper.GetMapData(m.Payload, "source")
-	if !ok {
-		log.Panicf("[%s] source is missing in parameters", driver.Service)
-	}
-	stype, ok := dipper.GetMapDataStr(source, "type")
-	if !ok {
-		log.Panicf("[%s] source type is missing in parameters", driver.Service)
-	}
-	log.Debugf("[%s] fetching k8config from source", driver.Service)
-	var kubeConfig *rest.Config
-	if stype == "gcloud-gke" {
-		kubeConfig = getGKEConfig(source.(map[string]interface{}))
-	} else {
-		log.Panicf("[%s] unsupported kubernetes source type: %s", driver.Service, stype)
-	}
-	if kubeConfig == nil {
-		log.Panicf("[%s] unable to get kubeconfig", driver.Service)
-	}
-
-	k8client, err := kubernetes.NewForConfig(kubeConfig)
-	if err != nil {
-		log.Panicf("[%s] unable to create k8 client", driver.Service)
 	}
 
 	rsclient := k8client.AppsV1().ReplicaSets(nameSpace)
@@ -84,6 +198,43 @@ func recycleDeployment(m *dipper.Message) {
 	}
 	log.Infof("[%s] deployment recycled %s.%s", driver.Service, nameSpace, rsName)
 	m.Reply <- dipper.Message{}
+}
+
+func prepareKubeConfig(m *dipper.Message) *kubernetes.Clientset {
+	if log == nil {
+		log = driver.GetLogger()
+	}
+	m = dipper.DeserializePayload(m)
+
+	source, ok := dipper.GetMapData(m.Payload, "source")
+	if !ok {
+		log.Panicf("[%s] source is missing in parameters", driver.Service)
+	}
+	stype, ok := dipper.GetMapDataStr(source, "type")
+	if !ok {
+		log.Panicf("[%s] source type is missing in parameters", driver.Service)
+	}
+	log.Debugf("[%s] fetching k8config from source", driver.Service)
+	var kubeConfig *rest.Config
+	if stype == "gcloud-gke" {
+		kubeConfig = getGKEConfig(source.(map[string]interface{}))
+	} else if stype == "local" {
+		kubeConfig, err = rest.InClusterConfig()
+		if err != nil {
+			log.Panicf("[%s] unable to load default account for kubernetes %+v", driver.Service, err)
+		}
+	} else {
+		log.Panicf("[%s] unsupported kubernetes source type: %s", driver.Service, stype)
+	}
+	if kubeConfig == nil {
+		log.Panicf("[%s] unable to get kubeconfig", driver.Service)
+	}
+
+	k8client, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		log.Panicf("[%s] unable to create k8 client", driver.Service)
+	}
+	return k8client
 }
 
 func getGKEConfig(cfg map[string]interface{}) *rest.Config {
