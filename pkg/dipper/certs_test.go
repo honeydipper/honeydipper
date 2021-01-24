48c979cb4c446c6be01e58bb24f9cf5379b10853
Chun Huang(Charles) <Charles546@users.noreply.github.com>
2021-01-23 16:09:43 -0800

    feat(drivers): redis drivers support TLS
    Supporting communicating to redis with TLS

---
+++ pkg/dipper/certs_test.go
@@ -0,0 +1,63 @@
+// Copyright 2021 Honey Science Corporation
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+// +build !integration
+
+package dipper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadCACerts(t *testing.T) {
+	certs := []interface{}{
+		`
+-----BEGIN CERTIFICATE-----
+MIIBuDCCAV4CCQDX6zn1LwUpWDAKBggqhkjOPQQDAjBkMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExFDASBgNVBAcMC0xvcyBBbmdlbGVzMRAwDgYDVQQKDAdDb21w
+YW55MRAwDgYDVQQLDAdTZWN0aW9uMQ4wDAYDVQQDDAVUZXN0MTAeFw0yMTAxMjQw
+MTIwNDFaFw0yMjAxMjQwMTIwNDFaMGQxCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJD
+QTEUMBIGA1UEBwwLTG9zIEFuZ2VsZXMxEDAOBgNVBAoMB0NvbXBhbnkxEDAOBgNV
+BAsMB1NlY3Rpb24xDjAMBgNVBAMMBVRlc3QxMFkwEwYHKoZIzj0CAQYIKoZIzj0D
+AQcDQgAELv64EsaVD0o3JJG/UMuVPmo570VN3hxtKB3nkJ3tNXYaurSgpe1AohMX
+/0avIuqnzlxM2JKkevYgW/HJyY+oVzAKBggqhkjOPQQDAgNIADBFAiBcDGOA7sy8
+MPZ+cqSWEXI4VAanhKCFxpN3urEb+mLtbAIhANjpn1Hjfbx5XSwWrHPfoCAoRzPd
+KiCFvgTSZQojAG8t
+-----END CERTIFICATE-----
+		`,
+		`
+-----BEGIN CERTIFICATE-----
+MIIBszCCAVgCCQCb9f3CUhy1wzAKBggqhkjOPQQDAjBhMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExETAPBgNVBAcMCFBhc2FkZW5hMRAwDgYDVQQKDAdDb21wYW55
+MRAwDgYDVQQLDAdTZWN0aW9uMQ4wDAYDVQQDDAVUZXN0MjAeFw0yMTAxMjQwMTIx
+NDBaFw0yMjAxMjQwMTIxNDBaMGExCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTER
+MA8GA1UEBwwIUGFzYWRlbmExEDAOBgNVBAoMB0NvbXBhbnkxEDAOBgNVBAsMB1Nl
+Y3Rpb24xDjAMBgNVBAMMBVRlc3QyMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE
+Lv64EsaVD0o3JJG/UMuVPmo570VN3hxtKB3nkJ3tNXYaurSgpe1AohMX/0avIuqn
+zlxM2JKkevYgW/HJyY+oVzAKBggqhkjOPQQDAgNJADBGAiEAwXclv5JcBBOAFcJ1
+mTh7GDlF5m4UBEQUAx4Sm4nVEAoCIQDoenOGwxdEvw5xbeZizct50bOZp1dU4u2c
+YYaBtD/3UQ==
+-----END CERTIFICATE-----
+-----BEGIN CERTIFICATE-----
+MIIBrTCCAVICCQC6ufl87XwWCjAKBggqhkjOPQQDAjBeMQswCQYDVQQGEwJVUzEL
+MAkGA1UECAwCQ0ExDjAMBgNVBAcMBUF6dXNhMRAwDgYDVQQKDAdDb21wYW55MRAw
+DgYDVQQLDAdTZWN0aW9uMQ4wDAYDVQQDDAVUZXN0MzAeFw0yMTAxMjQwMTIyMTha
+Fw0yMjAxMjQwMTIyMThaMF4xCzAJBgNVBAYTAlVTMQswCQYDVQQIDAJDQTEOMAwG
+A1UEBwwFQXp1c2ExEDAOBgNVBAoMB0NvbXBhbnkxEDAOBgNVBAsMB1NlY3Rpb24x
+DjAMBgNVBAMMBVRlc3QzMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAELv64EsaV
+D0o3JJG/UMuVPmo570VN3hxtKB3nkJ3tNXYaurSgpe1AohMX/0avIuqnzlxM2JKk
+evYgW/HJyY+oVzAKBggqhkjOPQQDAgNJADBGAiEA3wxNoM814xT/QVpUoQrmhurE
+s99PXe7pSqqKPGBq5Y0CIQCK367N+dWphmnMV4z3RO2loTPDlJaGh2Jz0VhQ4AE/
+Ow==
+-----END CERTIFICATE-----
+		`,
+	}
+	assert.NotEmpty(t, LoadCACerts(nil, true).Subjects(), "system cert pool should not be empty")
+	assert.Empty(t, LoadCACerts(nil, false).Subjects(), "pool should be empty")
+	assert.Equal(t, 3, len(LoadCACerts(certs, false).Subjects()), "should load 3 CA certs")
+}
